356f31d782c72fd10ba1b4aedad703be5876cbd0
hongnhan1 <hongnhan210127@gmail.com>
2024-11-01 11:30:11 +0700

    thêm lớp News

---
+++ KoiCareSystemAtHome.WebApplication/Models/News.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KoiCareSystemAtHome.WebApplication.Models
+{
+    public class News
+    {
+        [Key]
+        public string PostID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Author { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime PublishDate { get; set; }
+    }
+}
